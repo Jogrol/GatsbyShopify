063af18c68168dc3e5a581822006fd951bc227dc
Prive <prive@MacBook-Pro-van-Joey.local>
2020-10-04 21:29:14 +0200

    Changed banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,16 +2,21 @@ import React, { Children, useContext } from 'react'
 
 
 
+
 const Banner = () => {
-    return <div className='w-screen h-screen'>
-        <div className="max-w-5xl mx-auto flex justify-end items-center h-full">
+    return <div className='w-screen h-screen bg-fixed' style={{
+        backgroundImage: "url('../images/pexels-martin-péchy-1866149.jpg')"
+    }}
+    >
+        < div className="max-w-5xl mx-auto flex justify-end items-center h-full" >
+
             <div className="bg-gray-300 p-6 rounded-md">
-                <h1 className="text-6xl">Kuin Modern.</h1>
+                <h1 className="text-6xl leading-none">Producten te koop!</h1>
                 <p>
                     some paragraph text
                 </p>
             </div>
-        </div>
+        </div >
     </div >
 }
 
