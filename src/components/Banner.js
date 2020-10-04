15fb806bc2c247995275f6b8a6ac45bf902e2849
Prive <prive@MacBook-Pro-van-Joey.local>
2020-10-04 16:35:51 +0200

    Removed default styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,18 @@
-import React, { useContext } from 'react'
+import React, { Children, useContext } from 'react'
 
 
 
 const Banner = () => {
-    return <div className='w-full h-full'><h1>Hello</h1></div>
+    return <div className='w-screen h-screen'>
+        <div className="max-w-5xl mx-auto flex justify-end items-center h-full">
+            <div className="bg-gray-300 p-6 rounded-md">
+                <h1 className="text-6xl">Kuin Modern.</h1>
+                <p>
+                    some paragraph text
+                </p>
+            </div>
+        </div>
+    </div >
 }
 
 
---
+++ src/components/ProductGrid/ProductItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const ProductItem = (node) => {
+    console.log({ node })
+    // return <div key={id}>
+    //     <Link to={`/product/${handle}/`}>
+    //         {firstImage && firstImage.localFile && (
+    //             <Img
+    //                 fluid={firstImage.localFile.childImageSharp.fluid}
+    //                 alt={handle}
+    //             />
+    //         )}
+    //     </Link>
+    //     <Title>{title}</Title>
+    //     <PriceTag>{getPrice(firstVariant.price)}</PriceTag>
+    // </div>
+}
+
+export default ProductItem;
